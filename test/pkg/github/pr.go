a8db555d54b1a7af37e34f63ee1f4331275f6b80
Zaki Shaikh <zashaikh@redhat.com>
2026-04-07 12:12:34 +0530

    fix: PR close condition in e2e tests
    this fixes the if condition in TearDown func
    for GitHub.
    
    Signed-off-by: Zaki Shaikh <zashaikh@redhat.com>

--- test/pkg/github/pr.go
+++ test/pkg/github/pr.go
@@ -219,7 +219,7 @@ func (g *PRTest) TearDown(ctx context.Context, t *testing.T) {
 	// Collect GitHub API call information from controller logs
 	g.collectGitHubAPICalls(ctx, t)
 
-	if g.PRNumber != -1 && g.Provider != nil && g.Logger != nil {
+	if g.PRNumber > 0 && g.Provider != nil && g.Logger != nil {
 		g.Logger.Infof("Closing PR %d", g.PRNumber)
 		state := "closed"
 		_, _, err := g.Provider.Client().PullRequests.Edit(ctx,
