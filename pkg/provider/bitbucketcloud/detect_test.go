bf6521aabe724f28fce6c1d8220b155c3a1b1bb6
Zaki Shaikh <zashaikh@redhat.com>
2026-09-10 12:32:12 +0530

    fix(unittest): check event-id exist in logger in all providers
    event-id check is added in detect func unit test in all
    provider which silently succeeds the unit test if the
    key doesn't exist which is not intended. this commits
    adds a found boolean to verify that event-id key is found.
    
    Signed-off-by: Zaki Shaikh <zashaikh@redhat.com>

--- pkg/provider/bitbucketcloud/detect_test.go
+++ pkg/provider/bitbucketcloud/detect_test.go
@@ -163,16 +163,23 @@ func TestProviderDetect(t *testing.T) {
 				assert.Assert(t, logCatcher.FilterMessageSnippet(tt.wantLogSnippet).Len() > 0, logCatcher.All())
 			}
 
+			if !tt.isBC {
+				return
+			}
+
 			logger.Info("generate a log message to check if event-id is added to the logger")
 
+			found := false
 			logs := logCatcher.All()
 			for _, entry := range logs {
 				for _, field := range entry.Context {
 					if field.Key == "event-id" {
 						assert.Equal(t, field.String, "1234567890")
+						found = true
 					}
 				}
 			}
+			assert.Assert(t, found, "event-id not found in the logs")
 		})
 	}
 }
--- pkg/provider/gitea/detect_test.go
+++ pkg/provider/gitea/detect_test.go
@@ -211,16 +211,23 @@ func TestProviderDetect(t *testing.T) {
 			assert.Assert(t, isGitea == tt.isGitea)
 			assert.Assert(t, processEvent == tt.processEvent)
 
+			if !tt.isGitea {
+				return
+			}
+
 			logger.Info("generate a log message to check if event-id is added to the logger")
 
+			found := false
 			logs := logCatcher.All()
 			for _, entry := range logs {
 				for _, field := range entry.Context {
 					if field.Key == "event-id" {
 						assert.Equal(t, field.String, "1234567890")
+						found = true
 					}
 				}
 			}
+			assert.Assert(t, found, "event-id not found in the logs")
 		})
 	}
 }
